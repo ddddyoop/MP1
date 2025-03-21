2894526c449338225a26446b37f36f95e5256589
孙大卫 <dvia@Dvias-MacBook-Pro.local>
2025-03-21 01:30:38 +0100

    update name and haedline

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -3,12 +3,12 @@ export * from './education'
 export * from './career'
 
 // personal info
-export const name = 'Derek Hu'
-export const headline = 'Teacher at Shanghai Soong Ching Ling School.'
+export const name = 'Dawei Sun'
+export const headline = 'Designer who is studying in TU/e'
 export const introduction =
-  "Coucou Les Amis! My name is Derek Hu. I'm from Hubei and an alumnus of BUPT, UChicago, and Tsinghua. I’ve been teaching Computer Science at SCLS since 2020."
+  "Hello! My name is Dawei Sun. "
 export const email = 'tong.hu@scls-sh.org'
-export const githubUsername = 'derekhut'
+export const githubUsername = 'Dawei Sun'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
